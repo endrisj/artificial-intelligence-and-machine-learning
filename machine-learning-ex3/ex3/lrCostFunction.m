2116d80b147fec185d0ed017cbfbe548adf1fdde
Andrius Jukna <aj@platform-lunar.com>
2018-06-23 15:01:56 +0300

    Completed exercise 3

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -38,13 +38,10 @@ grad = zeros(size(theta));
 
 
 h = sigmoid(X * theta);
-J = (1/m)*sum(-y.*log(h) .- (1.-y).*log(1.-h))  + (lambda/(2*m) * (sum(theta(2:size(theta)).^2)));
+J = (1/m)*sum(-y.*log(h) .- (1.-y).*log(1.-h))  + (lambda/(2*m) * (sum(theta(2:end).^2)));
 
 grad(1) = ((1/m) .* (X'*(h .- y))) (1);
-grad(2:size(theta)) = ((1/m) .* (X'*(h .- y)))(2:size(theta))  + lambda/m * theta(2:size(theta));
-
-
-
+grad(2:size(theta)) = ((1/m) .* (X'*(h .- y)))(2:size(theta))  + lambda/m * theta(2:end);
 
 
 
--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -7,12 +7,12 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 p = zeros(size(X, 1), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
+%               your learned neural network. You should set p to a
 %               vector containing labels between 1 to num_labels.
 %
 % Hint: The max function might come in useful. In particular, the max
@@ -21,12 +21,19 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+% fprintf("BEG ?? \n");
+% Theta1
+% Theta2
+% fprintf("END ?? \n");
 
 
+X = [ones(m, 1) X];
+a2 = sigmoid(X * Theta1');
+a2 = [ones(size(a2, 1), 1) a2];
 
-
-
-
+for i=1:m
+  [value, p(i)] = max(sigmoid(a2(i,:) * Theta2'));
+end
 
 
 % =========================================================================
