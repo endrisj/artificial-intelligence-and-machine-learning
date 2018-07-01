b8ad73abcff25da7a316b593adef12493e23a7ed
Andrius Jukna <aj@platform-lunar.com>
2018-07-01 20:51:23 +0300

    Add regularization for NN backpropagation

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -97,11 +97,9 @@ reg = (lambda / (2 * m)) * (sum(sum(Theta1(:, 2:end) .^ 2)) + sum(sum(Theta2(:, 
 J += reg;
 
 
-%
-Theta1_grad = Theta1_grad / m;
-Theta2_grad = Theta2_grad / m;
-
-
+% backpropagation
+Theta1_grad = [Theta1_grad(:,1)/m ((Theta1_grad(:,2:end)/m) + (lambda/m) * Theta1(:,2:end))];
+Theta2_grad = [Theta2_grad(:,1)/m ((Theta2_grad(:,2:end)/m) + (lambda/m) * Theta2(:,2:end))];
 
 
 
